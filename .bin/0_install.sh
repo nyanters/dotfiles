1a0e99cd89d0ad34dfdfeadab8f70bd4c1357494
nyanters <123231885+nyanters@users.noreply.github.com>
2024-10-21 21:03:46 +0900

    modified:   .bin/0_install.sh new file:   .bin/2_link.sh new file:   .bin/2_link.txt

--- .bin/0_install.sh
+++ .bin/0_install.sh
@@ -3,4 +3,5 @@ set -eoux pipefail
 SCR_DIR=$(cd "$(dirname "$0")"; pwd)
 cd "${SCR_DIR}"
 source 1_os.sh
+source 2_link.sh
 exit 0
---
+++ .bin/2_link.sh
@@ -0,0 +1,76 @@
+#!usr/bin/env bash
+set -euxo pipefail
+readonly bak_dir="$HOME/.dotbackup"
+readonly script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+readonly dotdir=$(dirname ${script_dir})
+function cfg2homedir () {
+  for f in ${dotdir}/.config/**; do
+    ln -snf ${f} $HOME/.config/`basename ${f}`
+  done
+}
+function check_homedir () {
+  if [[ "$HOME" != "$dotdir" ]]; then
+    for f in $1/.??*; do
+      if [[ "${f}" == "$1/.config" ]]; then
+        continue
+      fi
+      if [[ -L "$2/`basename $f`" ]];then
+        command rm -f "$2/`basename $f`"
+      fi
+      if [[ -e "$2/`basename $f`" ]];then
+        command mv "$2/`basename $f`" "$3"
+      fi
+    done
+  else
+    command echo "same install src dest"
+  fi
+}
+function helpmsg () {
+  command echo "Usage: $0 [--help | -h]" 0>&2
+  command echo ""
+}
+function mkdir_w_mes () {
+  if [ ! -d "$1" ];then
+    command echo "$1 not found. Auto Make it"
+    command mkdir "$1"
+  fi
+}
+function link2homedir () {
+  command echo "backup old dotfiles..."
+  for f in $dotdir/.??*; do
+    for line in `cat "${script_dir}/2_link.txt"`; do
+      if [[ "$f" == "${dotdir}/${line}" ]]; then
+        continue 2
+      fi
+    done
+    command ln -snf $f $HOME
+  done
+}
+while [ $# -gt 0 ];do
+  case ${1} in
+    --debug|-d)
+      set -uex
+      ;;
+    --help|-h)
+      helpmsg
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
+function main_func () {
+  mkdir_w_mes "${bak_dir}"
+  check_homedir ${dotdir} $HOME ${bak_dir}
+  mkdir_w_mes "$HOME/.config"
+  link2homedir
+  check_homedir ${dotdir}.config $HOME/.config "${bak_dir}/.config"
+  cfg2homedir
+  git config --global include.path "~/.gitconfig_shared"
+  command echo -e "Install completed!!!!"
+}
+function main () {
+  main_func
+}
+main "$@"
