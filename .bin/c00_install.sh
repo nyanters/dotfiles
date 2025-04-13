5b9c140a3c0e29757aeab4e155514e8dc889fd22
nyanters <123231885+nyanters@users.noreply.github.com>
2025-04-13 11:44:49 +0900

    modified:   c00_install.sh

--- .bin/c00_install.sh
+++ .bin/c00_install.sh
@@ -2,7 +2,7 @@
 set -eoux pipefail
 SCR_DIR=$(cd "$(dirname "$0")"; pwd)
 cd "${SCR_DIR}"
-# source c01_os.sh
+source c01_os.sh
 source c02_link.sh
 if [[ "$(uname)" == "Darwin" ]]; then
   source m01_init.sh
@@ -11,5 +11,5 @@ if [[ "$(uname)" == "Darwin" ]]; then
   source m04_brew.sh
   source m05_lporg.sh
 fi
-source c03_uv.sh
+# source c03_uv.sh
 exit 0
