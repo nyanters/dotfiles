420cd062512fa5d4c62bf0a987af391b9a76b1fe
nyanters <123231885+nyanters@users.noreply.github.com>
2025-05-31 17:34:20 +0900

    modified p01_pacman.sh

--- .bin/p01_pacman.sh
+++ .bin/p01_pacman.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cat "$(basename ${BASH_SOURCE[0]} .sh).txt" | xargs pacman -S --needed --no-confirm
+cat "$(basename ${BASH_SOURCE[0]} .sh).txt" | xargs pacman -S --needed --noconfirm
 return 0
