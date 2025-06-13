92e3a746048b721aad28a020d5f5667d14ebd735
nyanters <123231885+nyanters@users.noreply.github.com>
2025-06-13 18:10:45 +0900

    modified .Brewfile

--- .bin/m01_init.sh
+++ .bin/m01_init.sh
@@ -19,20 +19,4 @@ if ! xcode-select --print-path &> /dev/null; then
 else
   echo "Command line tools are already installed."
 fi
-
-# Install Rosetta 2 for Apple Silicon
-# Check if Rosetta 2 is installed
-if [[ $(sysctl -n machdep.cpu.brand_string) != *"Apple M"* ]]; then
-  echo "Rosetta 2 is already installed."
-  return 0
-fi
-
-# Install Rosetta 2
-/usr/sbin/softwareupdate --install-rosetta --agree-to-license
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to install Rosetta 2."
-  exit 1
-fi
-
-echo "Rosetta 2 has been installed successfully."
 return 0
