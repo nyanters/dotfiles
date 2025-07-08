9730a1ca9d45303fa63ff01ea6537555b3e63b1c
nyanters <123231885+nyanters@users.noreply.github.com>
2025-07-08 19:33:32 +0900

    modified:   .bin/m04_brew.sh modified:   ref/ref.tsv

--- .bin/m04_brew.sh
+++ .bin/m04_brew.sh
@@ -10,18 +10,18 @@ fi
 select val in ${MENU}; do
   case ${val} in
     minimal)
-      brew bundle --file ~/.Brewfile_basic
+      brew bundle cleanup -f --file ~/.Brewfile_basic
       break
       ;;
     mas | rosetta)
       cat ~/.Brewfile_basic ~/.Brewfile_${val} >> ~/.Brewfile
-      brew bundle --global
+      brew bundle cleanup -f --global
       rm ~/.Brewfile
       break
       ;;
     full)
       cat ~/.Brewfile_* >> ~/.Brewfile
-      brew bundle --global
+      brew bundle cleanup -f --global
       rm ~/.Brewfile
       break
       ;;
