847a6ad66687dec3fe605063e65dd07f1dbf7034
nyanters <123231885+nyanters@users.noreply.github.com>
2025-07-09 10:26:00 +0900

    modified:   .Brewfile_basic modified:   .bin/m51_save.sh

--- .bin/m51_save.sh
+++ .bin/m51_save.sh
@@ -1,10 +1,12 @@
 #!usr/bin/env bash
 brew bundle dump -f --global
 csplit -f ~/temp -n 4 ~/.Brewfile /^mas/ {0}
-mv ~/temp0000 ${dotdir}/.Brewfile_basic
+mv ~/temp0000 ${dotdir}/.Brewfile_basic.bak
 mv ~/temp0001 ${dotdir}/.Brewfile_mas
 rm -f ~/.Brewfile
-for line in ~/.Brewfile_rosetta; do
-  grep -v ${line} ~/.Brewfile_basic >> ${dotdir}/.Brewfile_basic
+for line in ${dotdir}/.Brewfile_rosetta; do
+  grep -v ${line} ${dotdir}/.Brewfile_basic.bak > ${dotdir}/.Brewfile_basic
 done
+rm -f ${dotdir}/.Brewfile_basic
+mv ${dotdir}/.Brewfile_basic.bak ${dotdir}/.Brewfile_basic
 exit 0
