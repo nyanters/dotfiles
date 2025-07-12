c7c0c6efeae0eb18a6035064a17d4d2774217eb6
nyanters <123231885+nyanters@users.noreply.github.com>
2025-07-12 15:05:55 +0900

    modified m51_save.sh

--- .bin/m51_save.sh
+++ .bin/m51_save.sh
@@ -4,9 +4,10 @@ csplit -f ~/temp -n 4 ~/.Brewfile /^mas/ {0}
 mv ~/temp0000 ${dotdir}/.Brewfile_basic.bak
 mv ~/temp0001 ${dotdir}/.Brewfile_mas
 rm -f ~/.Brewfile
-for line in ${dotdir}/.Brewfile_rosetta; do
-  grep -v ${line} ${dotdir}/.Brewfile_basic.bak > ${dotdir}/.Brewfile_basic
-done
-rm -f ${dotdir}/.Brewfile_basic
+while read line; do
+  grep -v "${line}" ${dotdir}/.Brewfile_basic.bak > ${dotdir}/.Brewfile_basic
+  rm -f ${dotdir}/.Brewfile_basic.bak
+  mv ${dotdir}/.Brewfile_basic ${dotdir}/.Brewfile_basic.bak
+done < ${dotdir}/.Brewfile_rosetta
 mv ${dotdir}/.Brewfile_basic.bak ${dotdir}/.Brewfile_basic
 exit 0
