ca7b897be80426df2e914fc470da5f35ddf05702
nyanters <123231885+nyanters@users.noreply.github.com>
2025-07-14 07:55:48 +0900

    modified:   .bin/m51_save.sh

--- .bin/m51_save.sh
+++ .bin/m51_save.sh
@@ -4,18 +4,9 @@ csplit -f ~/temp -n 4 ~/.Brewfile /^mas/ {0}
 mv ~/temp0000 ${dotdir}/.Brewfile_basic
 mv ~/temp0001 ${dotdir}/.Brewfile_mas
 rm -f ~/.Brewfile
-<<<<<<< HEAD
-for line in ${dotdir}/.Brewfile_rosetta; do
-  grep -v ${line} ${dotdir}/.Brewfile_basic > ${dotdir}/.Brewfile_basic.bak
+while read line; do
+  grep -v "${line}" ${dotdir}/.Brewfile_basic > ${dotdir}/.Brewfile_basic
   rm -f ${dotdir}/.Brewfile_basic
   mv ${dotdir}/.Brewfile_basic.bak ${dotdir}/.Brewfile_basic
-done
-=======
-while read line; do
-  grep -v "${line}" ${dotdir}/.Brewfile_basic.bak > ${dotdir}/.Brewfile_basic
-  rm -f ${dotdir}/.Brewfile_basic.bak
-  mv ${dotdir}/.Brewfile_basic ${dotdir}/.Brewfile_basic.bak
 done < ${dotdir}/.Brewfile_rosetta
-mv ${dotdir}/.Brewfile_basic.bak ${dotdir}/.Brewfile_basic
->>>>>>> refs/remotes/origin/main
 exit 0
