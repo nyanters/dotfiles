21ee93553dfe8dc233ae27f268b79743fd553e2d
nyanters <123231885+nyanters@users.noreply.github.com>
2025-07-18 09:31:22 +0900

    modified:   .Brewfile_basic modified:   .bin/m51_save.sh modified:   .config/btop/btop.conf new file:   .zsh/nodebrew.zsh

--- .bin/m51_save.sh
+++ .bin/m51_save.sh
@@ -7,7 +7,7 @@ mv ~/temp0001 ${dotdir}/.Brewfile_mas
 rm -f ~/.Brewfile
 for i in "${ARRAY[@]}"; do
   while read line; do
-    grep -v "${line}" ${dotdir}/.Brewfile_basic > ${dotdir}/.Brewfile_basic
+    grep -v "${line}" ${dotdir}/.Brewfile_basic > ${dotdir}/.Brewfile_basic.bak
     rm -f ${dotdir}/.Brewfile_basic
     mv ${dotdir}/.Brewfile_basic.bak ${dotdir}/.Brewfile_basic
   done < ${dotdir}/.Brewfile_${i}
