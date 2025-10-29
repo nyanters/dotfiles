b4e61cc3a44c0af179e728cd4f29c28c5363ba6b
nyanters <123231885+nyanters@users.noreply.github.com>
2025-10-29 12:28:09 +0900

    modified:   .bash/bitwarden.bash

--- .bash/bitwarden.bash
+++ .bash/bitwarden.bash
@@ -1,4 +1,5 @@
 #!usr/bin/env bash
 if [[ "$(uname)" == "Linux" ]]; then
   export SSH_AUTH_SOCK=${HOME}/.bitwarden-ssh-agent.sock
+  export SSH_AUTH_SOCK=${HOME}/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
 fi
